aa9891d0238d1324b96a8b955c58ec29614c9386
nkn-nkn <nayanm786@gmail.com>
2017-07-10 17:47:54 +0530

    Update settings.py

--- Portal/settings.py
+++ Portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kp60&-i%)ye3u1(cdi356s_&lgvljn%-@tw_*jyepppv*dp+g&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.137.1']
+ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'placementportaliet@gmail.com'
